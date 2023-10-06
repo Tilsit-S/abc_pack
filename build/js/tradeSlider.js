1d8498d9bab80f701bdc3f247e5b5a9ac94f260f
Tilzit <mylittleanabolik@gmail.com>
2023-10-06 18:08:32 +0200

    form

--- build/js/tradeSlider.js
+++ build/js/tradeSlider.js
@@ -1,23 +1,25 @@
-"use strict";
-
 var slider = document.querySelector('.tarde-slider');
 var prevButton = document.querySelector('.prev-button');
 var nextButton = document.querySelector('.next-button');
 var slides = Array.from(slider.querySelectorAll('.slider-item'));
 var slideCount = slides.length;
-var slideIndex = 0;
+let slideIndex = 0;
+
 prevButton.addEventListener('click', showPreviousSlide);
 nextButton.addEventListener('click', showNextSlide);
+
 function showPreviousSlide() {
   slideIndex = (slideIndex - 1 + slideCount) % slideCount;
   updateSlider();
 }
+
 function showNextSlide() {
   slideIndex = (slideIndex + 1) % slideCount;
   updateSlider();
 }
+
 function updateSlider() {
-  slides.forEach(function (slide, index) {
+  slides.forEach((slide, index) => {
     if (index === slideIndex) {
       slide.style.display = 'block';
     } else {
@@ -25,4 +27,5 @@ function updateSlider() {
     }
   });
 }
+
 updateSlider();
