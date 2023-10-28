2bfc8e8ddb2cc939e637cc5c733c734ff00ac74f
Tilzit <mylittleanabolik@gmail.com>
2023-10-28 23:16:23 +0200

    remark tabs

--- source/js/tradeSlider.js
+++ source/js/tradeSlider.js
@@ -1,31 +1,8 @@
-/*var slider = document.querySelector('.tarde-slider');
-var prevButton = document.querySelector('.prev-button');
-var nextButton = document.querySelector('.next-button');
-var slides = Array.from(slider.querySelectorAll('.slider-item'));
-var slideCount = slides.length;
-let slideIndex = 0;
-
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
-}
-
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        variableWidth: false,
     }
-  });
-}
-
-updateSlider();*/
+    );
+});
