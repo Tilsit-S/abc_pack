6790fb1732d6c8f1231505ffa4d00b372d8cb2f9
Tilzit <mylittleanabolik@gmail.com>
2023-10-30 15:51:33 +0200

    fixed text from packing button

--- source/js/packingButton.js
+++ source/js/packingButton.js
@@ -1,6 +1,7 @@
-$(document).ready(function() { 
-    $("button").click(function() { 
-        // Change text of button element 
-        $("#packing-button").html("Скрыть");
+$(function(){
+    $(".packing__showing-link").click(function () {
+       $(this).text(function(i, text){
+           return text === "Смотреть ещё" ? "Свернуть" : "Смотреть ещё";
+       })
     });
-});
+ })
